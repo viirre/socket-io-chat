0d8534c8c1212e6d01b5942a4d778fbec65c9a90
Victor Schelin <victor@adaptivemedia.se>
2015-10-02 16:24:38 +0200

    fixed user list with avatar

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ app.get('/', function(req, res){
   res.sendfile('chat.html');
 });
 
+
 app.use("/css", express.static(__dirname + '/css'));
+app.use("/js", express.static(__dirname + '/js'));
+
+
 
 io.on('connection', function(socket){
   	
@@ -22,25 +26,43 @@ io.on('connection', function(socket){
 
   	socket.on('disconnect', function(){
     	console.log('a user disconnected :/', socket.username);
-      //delete users[socket.username];
-      users.splice(users.indexOf(socket.username), 1);
+      removeUser(socket.username);
       console.log("emitting new userlist", users);
       io.emit('chat.users', users);
   	});
 
   	socket.on('chat.message', function(msg){
 	    console.log('message: ' + msg);
+      var email = msg.email;
+      msg.user = userFromEmail(email);
 	    messages.push(msg);
 	    io.emit('chat.message', msg);
   	});
 
     socket.on('chat.user', function(user){
-      console.log('new user added: ' + user);
+      console.log('new user added: ', user);
       users.push(user);
-      socket.username = user;
+      socket.username = user.email;
+      console.log("emitting new userlist", users);
       io.emit('chat.users', users);
     });
 });
+
+function userFromEmail(email) {
+  for(var i in users) {
+    if (users[i].email == email) {
+      return users[i];
+    }
+  }
+}
+
+function removeUser(email) {
+  for(var i in users) {
+    if (users[i].email == email) {
+      users.splice(i, 1);
+    }
+  }
+}
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
